0a5f43dff22a34838e73644951ae853145eb62c1
Conor Higgins <wodowods@gmail.com>
2022-09-08 10:47:04 +0100

    HSL controls for colour blocks

--- src/components/ColourBlock/ColourBlock.jsx
+++ src/components/ColourBlock/ColourBlock.jsx
@@ -1,23 +1,53 @@
 import { useState } from "react";
 import HueBlock from "../HueBlock/HueBlock";
+import Button from "../../components/Button/Button";
 import "./colourBlock.scss";
 
 const ColourBlock = ({ colour, animation, hexToRGB, RGBtoHSL, HSLtoRGB }) => {
     const [colourBG, setColourBG] = useState(colour);
     const [hues, toggleHues] = useState(false);
+    const [hsl, setHsl] = useState("");
     return (
-        <div
-            className="colour-block-container"
-            onClick={() => {
-                toggleHues(!hues);
-            }}
-        >
-            {hues && <HueBlock baseCol={colourBG} setColourBG={setColourBG} />}
+        <div className="colour-block-container">
+            {hues && (
+                <HueBlock
+                    baseCol={colourBG}
+                    setColourBG={setColourBG}
+                    hsl={hsl}
+                />
+            )}
             <div
                 className="colour-block"
                 style={{ backgroundColor: colourBG, animation: animation }}
+                onMouseLeave={() => {
+                    toggleHues(false);
+                }}
             >
                 <p className="colour-block-properties">{colourBG}</p>
+                <Button
+                    classN="colour-block"
+                    value="hue"
+                    onClick={() => {
+                        setHsl("h");
+                        toggleHues(!hues);
+                    }}
+                />
+                <Button
+                    classN="colour-block"
+                    value="sat"
+                    onClick={() => {
+                        setHsl("s");
+                        toggleHues(!hues);
+                    }}
+                />
+                <Button
+                    classN="colour-block"
+                    value="lum"
+                    onClick={() => {
+                        setHsl("l");
+                        toggleHues(!hues);
+                    }}
+                />
                 <p className="colour-block-properties">
                     {hexToRGB(colourBG).join()}
                 </p>
--- src/components/HueBlock/HueBlock.jsx
+++ src/components/HueBlock/HueBlock.jsx
@@ -6,13 +6,13 @@ import {
 } from "../../API/PaletteService";
 import "./HueBlock.scss";
 
-const HueBlock = ({ baseCol, setColourBG }) => {
+const HueBlock = ({ baseCol, setColourBG, hsl }) => {
     const huesArray = (col) => {
         let hues = [];
         const colRGB = hexToRGB(col);
         for (let i = 0; i < 15; i++) {
             const colHSL = RGBtoHSL(colRGB[0], colRGB[1], colRGB[2]);
-            const hueRGB = HSLtoRGB(colHSL[0], colHSL[1], i * (255 / 40));
+            const hueRGB = HSLtoRGB(i * (360 / 15), colHSL[1], colHSL[2]);
             const finalCol = rgbToHex(hueRGB[0], hueRGB[1], hueRGB[2]);
             hues[i] = (
                 <div
@@ -27,7 +27,55 @@ const HueBlock = ({ baseCol, setColourBG }) => {
 
         return hues;
     };
-    return <div className="hue-block">{huesArray(baseCol)}</div>;
+
+    const satArray = (col) => {
+        let sats = [];
+        const colRGB = hexToRGB(col);
+        for (let i = 0; i < 15; i++) {
+            const colHSL = RGBtoHSL(colRGB[0], colRGB[1], colRGB[2]);
+            const hueRGB = HSLtoRGB(colHSL[0], i * (255 / 40), colHSL[2]);
+            const finalCol = rgbToHex(hueRGB[0], hueRGB[1], hueRGB[2]);
+            sats[i] = (
+                <div
+                    className="hue-slice"
+                    style={{ backgroundColor: finalCol }}
+                    onClick={() => {
+                        setColourBG(finalCol);
+                    }}
+                ></div>
+            );
+        }
+
+        return sats;
+    };
+
+    const lightnessArray = (col) => {
+        let lights = [];
+        const colRGB = hexToRGB(col);
+        for (let i = 0; i < 15; i++) {
+            const colHSL = RGBtoHSL(colRGB[0], colRGB[1], colRGB[2]);
+            const hueRGB = HSLtoRGB(colHSL[0], colHSL[1], i * (255 / 40));
+            const finalCol = rgbToHex(hueRGB[0], hueRGB[1], hueRGB[2]);
+            lights[i] = (
+                <div
+                    className="hue-slice"
+                    style={{ backgroundColor: finalCol }}
+                    onClick={() => {
+                        setColourBG(finalCol);
+                    }}
+                ></div>
+            );
+        }
+
+        return lights;
+    };
+
+    if (hsl == "h")
+        return <div className="hue-block">{huesArray(baseCol)}</div>;
+    else if (hsl == "s")
+        return <div className="hue-block">{satArray(baseCol)}</div>;
+    else if (hsl == "l")
+        return <div className="hue-block">{lightnessArray(baseCol)}</div>;
 };
 
 export default HueBlock;
